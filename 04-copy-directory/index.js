621dc8142893ec35e95e85103136077499690b6d
VeronikaNemirova <nvn92@tut.by>
2022-11-03 18:09:13 +0300

    fix: fix mistake with deleted files in 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 
 async function copyDir(dirPath) {
+    await fs.promises.rmdir(dirPath + '-copy', {recursive: true});
     const dir = await fs.promises.mkdir(dirPath + '-copy', {recursive: true});
     const files = await fs.promises.readdir(dirPath);
     for (const file of files) {
